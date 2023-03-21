7faa29eae48aa2f8287e6c06b66646ea3a8cb367
Tutiuna <boldinskaya2014@yandex.ru>
2023-03-21 23:14:07 +0300

    Add new file

---
+++ POST.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+info = {
+  "id": 0,
+  "category": {
+    "id": 0,
+    "name": "string"
+  },
+  "name": "Bingo",
+  "photoUrls": [
+    "Bingo's photo"
+  ],
+  "tags": [
+    {
+      "id": 0,
+      "name": "string"
+    }
+  ],
+  "status": "available"
+}
+res = requests.post(f"https://petstore.swagger.io/v2/pet", headers={'accept': 'application/json', 'Content-Type': 'application/json'},
+                         data=json.dumps(info, ensure_ascii=False))
+
+print(res.json)
+print(res.status_code)
